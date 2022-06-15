8d407bac45a6f1be3ae62eaa5c37f52c8600387d
Gabriel Batista <54750556+GabrielBN123@users.noreply.github.com>
2022-06-15 12:50:43 -0400

    .

--- .phan/config.php
+++ .phan/config.php
@@ -9,35 +9,12 @@ return [
         'vendor/symfony/dom-crawler',
         'vendor/guzzlehttp',
     ],
-
-    // A directory list that defines files that will be excluded
-    // from static analysis, but whose class and method
-    // information should be included.
-    //
-    // Generally, you'll want to include the directories for
-    // third-party code (such as "vendor/") in this list.
-    //
-    // n.b.: If you'd like to parse but not analyze 3rd
-    //       party code, directories containing that code
-    //       should be added to the `directory_list` as
-    //       to `exclude_analysis_directory_list`.
+    
     "exclude_analysis_directory_list" => [
         'vendor/'
     ],
 
-    // A list of plugin files to execute.
-    // Plugins which are bundled with Phan can be added here by providing their name
-    // (e.g. 'AlwaysReturnPlugin')
-    //
-    // Documentation about available bundled plugins can be found
-    // at https://github.com/phan/phan/tree/v5/.phan/plugins
-    //
-    // Alternately, you can pass in the full path to a PHP file
-    // with the plugin's implementation.
-    // (e.g. 'vendor/phan/phan/.phan/plugins/AlwaysReturnPlugin.php')
     'plugins' => [
-        // checks if a function, closure or method unconditionally returns.
-        // can also be written as 'vendor/phan/phan/.phan/plugins/AlwaysReturnPlugin.php'
         'AlwaysReturnPlugin',
         'DollarDollarPlugin',
         'DuplicateArrayKeyPlugin',
@@ -45,8 +22,7 @@ return [
         'PregRegexCheckerPlugin',
         'PrintfCheckerPlugin',
         'SleepCheckerPlugin',
-        // Checks for syntactically unreachable statements in
-        // the global scope or function bodies.
+        
         'UnreachableCodePlugin',
         'UseReturnValuePlugin',
         'EmptyStatementListPlugin',
